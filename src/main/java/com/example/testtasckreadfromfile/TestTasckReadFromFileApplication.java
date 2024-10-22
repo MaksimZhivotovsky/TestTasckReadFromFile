fbad09885d751cbffdaa57cc102e945b4ba91b27
M_Zhivotovskiy <M_Zhivotovskiy@step.ru>
2024-10-22 20:04:58 +0300

    init branch

--- src/main/java/com/example/testtasckreadfromfile/TestTasckReadFromFileApplication.java
+++ src/main/java/com/example/testtasckreadfromfile/TestTasckReadFromFileApplication.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 @SpringBootApplication
 public class TestTasckReadFromFileApplication {
 
-//    C:\Users\maksi\IdeaProjects\TestTasckReadFromFile\TestTasckReadFromFile\src\main\resources\WorkbookToArray.xlsx
     public static void main(String[] args) throws IOException {
         SpringApplication.run(TestTasckReadFromFileApplication.class, args);
 
